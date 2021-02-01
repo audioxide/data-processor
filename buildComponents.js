0df7c4c5ece0c9ab80a23cf2206c3916a3da81fe
Andrew Bridge <andrew@andrewhbridge.co.uk>
2021-02-01 07:26:42 +0000

    Remove commented Rollup code

--- buildComponents.js
+++ buildComponents.js
@@ -2,60 +2,6 @@ const fs = require('fs');
 const nodePath = require('path');
 const { default: Parcel } = require('@parcel/core');
 
-/* const DEFAULT_INPUT_OPTIONS = {
-	plugins: [
-		resolve({ preferBuiltins: true }),
-		commonjs(),
-		json(),
-		injectProcessEnv({}),
-		terser(),
-	],
-};
-
-const DEFAULT_OUTPUT_OPTIONS = {
-    format: 'iife',
-};
-
-const getInputFile = (componentDirectory) => nodePath.resolve(path + '/component.js');
-const getConfigFile = (componentDirectory) => nodePath.resolve(path + '/rollup.config.js');
-
-const getUserConfig = async (path, outputFilePath) => {
-    const { options, warnings } = await loadConfigFile(
-        getConfigFile(path),
-        {
-            input: getInputFile(path),
-            file: nodePath.resolve(outputFilePath),
-        },
-    );
-
-    console.log(`We currently have ${warnings.count} warnings`);
-    warnings.flush();
-
-    return options;
-}
-
-const getDefaultConfig = (path, outputFilePath) => new Promise((resolve) => resolve([
-    {
-        input: getInputFile(path),
-        output: [
-            {
-                file: nodePath.resolve(outputFilePath),
-                ...DEFAULT_OUTPUT_OPTIONS,
-            },
-        ],
-        ...DEFAULT_INPUT_OPTIONS,
-    },
-]));
-
-export default async (componentDirectory, outputFilePath) => {
-    const configResolver = fs.existsSync(getConfigFile(componentDirectory)) ? getUserConfig : getDefaultConfig;
-    const options = await configResolver(componentDirectory, outputFilePath);
-    for (const optionsObj of options) {
-        const bundle = await rollup.rollup(optionsObj);
-        await Promise.all(optionsObj.output.map(bundle.write));
-    }
-} */
-
 module.exports = class ComponentBundler {
     entries = [];
 
