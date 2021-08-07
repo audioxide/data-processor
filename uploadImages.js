93b528db2c9cc43f0f6aa1b2744c904bd4491f7c
Andrew Bridge <andrew@andrewhbridge.co.uk>
2021-08-07 23:43:12 +0100

    Add ImageKit uploader

---
+++ uploadImages.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const ImageKit = require("imagekit");
+const fs = require('fs/promises');
+const nodePath = require('path');
+
+const {
+    IMAGEKIT_PUBKEY,
+    IMAGEKIT_PRIVKEY,
+    IMAGEKIT_URL
+} = process.env;
+
+const client = new ImageKit({
+    publicKey: IMAGEKIT_PUBKEY,
+    privateKey: IMAGEKIT_PRIVKEY,
+    urlEndpoint: IMAGEKIT_URL
+});
+
+const inputBase = './data/images';
+const parseDir = async (path) => {
+    const files = await fs.readdir(inputBase + path);
+    await Promise.all(files.map(async file => {
+        const filePath = `${path}/${file}`;
+        const stat = await fs.stat(inputBase + filePath);
+        if (stat.isDirectory()) {
+            await parseDir(filePath);
+        } else if (stat.isFile()) {
+            await processFile(filePath);
+        }
+    }));
+};
+
+const remoteFiles = [];
+const processFile = async (path) => {
+    if (remoteFiles.includes(path)) return; // Image does not need processing
+    const { dir, base } = nodePath.parse(path);
+    await client.upload({
+        useUniqueFileName: false,
+        folder: dir,
+        fileName: base,
+        file: await fs.readFile(inputBase + path),
+    });
+};
+
+(async () => {
+    remoteFiles.push(...(await client.listFiles()).map(file => file.filePath));
+    await parseDir('');
+})()
